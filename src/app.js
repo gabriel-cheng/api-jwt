2f2ceaaf800407adce098eaf2095b07134eb4b57
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2022-12-12 03:44:26 -0300

    :tada: first commit :fire:

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const router = require('./router/index.router');
+
+app.use(express.json());
+
+app.use(router);
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+const app = require('./app');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
